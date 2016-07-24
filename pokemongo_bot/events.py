c35f2108fbf678b9ea8fb364fc8d7348c6060efc
James Droste <james@droste.im>
2016-07-24 22:38:07 +0100

    Stupid unicode

--- pokemongo_bot/events.py
+++ pokemongo_bot/events.py
@@ -1,24 +1,28 @@
 # -*- coding: utf-8 -*-
 
 class EventManager(object):
-​
+
     def __init__(self, bot):
         self.bot = bot
         self.event_handle_dict = {}
-​
+        self._init_event_handlers()
+
+    def _init_event_handlers(self):
+        pass
+
     def register_handler(self, event, handler_class):
         handler_list = self.event_handle_dict.get(event, [])
         handler_list += handler_class
-​
+
     def emit_event(self, event_name, **kwargs):
         handlers = self.event_handle_dict.get(event_name, [])
-        
+
         for handler in handlers:
             handler_object = handler(self, **kwargs)
             handler_object.process()
-​
+
 class IEventHandler(object):
-    
+
     def __init__(self, bot, event_manager):
         self.bot = bot
         self.event_manager = event_manager
